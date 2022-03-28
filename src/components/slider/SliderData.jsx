9c46966b034ce9fb8206b3ef38913ff2b165dfb2
alcun <alasdair@talkwell.world>
2022-03-28 03:52:15 +0100

    updated sliderData

--- src/components/slider/SliderData.jsx
+++ src/components/slider/SliderData.jsx
@@ -31,7 +31,7 @@ export const SliderData = [
     tech2: "Styled Components",
     tech3: "Trafft Scheduling",
     link: "https://keystone-therapies.co.uk",
-    repoLink: "",
+    repoLink: "https://https://github.com/alcun/Keystone",
   },
   {
     id: 2,
